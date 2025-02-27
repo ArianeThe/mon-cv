395b78b1e61643eb187297e6ccf13136b0ad6715
Ariane Thebault <coppey.ariane@gmail.com>
2025-02-27 09:52:06 +0100

    maj responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,53 @@
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Navbar.scss';
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <NavLink to="/" className="nav-brand">
-          THEBAULT Ariane
-        </NavLink>
-        <div className="nav-links">
-          <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>
+
+          <Link to="/" className="nav-brand" onClick={closeMenu}>
+          Portfolio
+        </Link>
+
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </button>
+
+        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+
+          <NavLink to="/" onClick={closeMenu}>
             Accueil
           </NavLink>
-          <NavLink to="/experience" className={({ isActive }) => isActive ? 'active' : ''}>
+
+          <NavLink to="/experience" onClick={closeMenu}>
             Expériences
           </NavLink>
-          <NavLink to="/competences" className={({ isActive }) => isActive ? 'active' : ''}>
+
+          <NavLink to="/competences" onClick={closeMenu}>
             Compétences
           </NavLink>
-          <NavLink to="/formation" className={({ isActive }) => isActive ? 'active' : ''}>
+
+         <NavLink to="/formation" onClick={closeMenu}>
             Formations
           </NavLink>
-          <NavLink to="/contact" className={({ isActive }) => isActive ? 'active' : ''}>
+
+          <NavLink to="/contact" onClick={closeMenu}>
             Contact
           </NavLink>
         </div>
