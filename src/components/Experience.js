3bd501ba10cfc7353f5df2da6a8726d5002aaa2d
Ariane Thebault <coppey.ariane@gmail.com>
2025-03-04 09:22:12 +0100

    ajout exériences

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,6 +7,19 @@ function Experience() {
     {
       id: 1,
       poste: "Conceptrice Développeuse d'Applications",
+      entreprise: "Projet Personnel",
+      periode: "01/2025 - ce jour",
+      lieu: "Casson",
+      description: [
+        "Conception d'un site de présentation de films et séries, servant également de bac à sable",
+        "Utilsation du site comme outils de montée en compétences",
+        "Développement de la partie Front-End et Back End"
+      ],
+      technologies: [, "Java", "Spring", "Thymleaf"]
+    },
+    {
+      id: 2,
+      poste: "Conceptrice Développeuse d'Applications",
       entreprise: "Mismo",
       periode: "12/2023 - 05/2024",
       lieu: "La Chapelle sur Erdre",
@@ -20,7 +33,7 @@ function Experience() {
       technologies: ["React", "Java", "Spring", "SQLServer"]
     },
     {
-        id: 2,
+        id: 3,
         poste: "Conceptrice Développeuse d'Applications",
         entreprise: "Projet personnel",
         periode: "12/2024",
@@ -34,7 +47,7 @@ function Experience() {
         technologies: ["Java", "Spring", "Thymleaf"]
       },
     {
-      id: 3,
+      id: 4,
       poste: "Conceptrice Développeuse d'Applications",
       entreprise: "Projet personnel",
       periode: "02/2024",
@@ -48,7 +61,7 @@ function Experience() {
       technologies: ["PHP", "Symfony", "CSS", "JS"]
     },
     {
-        id: 4,
+        id: 5,
         poste: "Conceptrice Développeuse d'Applications",
         entreprise: "Projet personnel",
         periode: "01/2023 - 02/2023",
@@ -64,7 +77,7 @@ function Experience() {
         technologies: ["PHP", "Symfony", "MySQL", "JS"]
       },
       {
-        id: 5,
+        id: 6,
         poste: "Développeuse Web et Web Mobile",
         entreprise: "InterHop",
         periode: "10/2022 - 11/2022",
@@ -78,7 +91,7 @@ function Experience() {
         technologies: ["PHP", "Symfony", "MySQL", "GitLab"]
       },
       {
-        id: 6,
+        id: 7,
         poste: "Assistante Administrative",
         entreprise: "Synergie - Anvolia - Océa Smart Building",
         periode: "10/2017 - 01/2022",
@@ -91,7 +104,7 @@ function Experience() {
         technologies: ["Word", "Excel", "ERP maison"]
       },
       {
-        id: 6,
+        id: 8,
         poste: "Commis de cuisine et Assistante manager en restauration rapide",
         entreprise: "Quick - Stratto - La Cabane",
         periode: "1997 - 2014",
